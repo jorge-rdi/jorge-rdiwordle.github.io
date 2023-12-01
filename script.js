48319da233addcfe183d32ad440dae0bae1e4c87
jorge-rdi <144245705+jorge-rdi@users.noreply.github.com>
2023-12-01 02:28:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const DICCIONARIO = ["CIELO", "AGUAS", "NIEVE", "LEJOS", "BUZON", "JUEGO", "PIEZA", "FUEGO", "HIELO", "PERRO", "ROLLO", "ROMPE", "BANCO", "JURAS", "RISAS", "TELAS", "BAILE"];
 let PALABRA = DICCIONARIO[Math.floor(Math.random() * DICCIONARIO.length)];
-
 let intentos = 6;
 console.log(PALABRA);
 
@@ -10,117 +9,115 @@ const INPUT = document.getElementById("guess-input");
 
 
 INPUT.addEventListener("input", () => {
-  if (INPUT.value.trim() !== "") {
-    BUTTON.disabled = false;
-  } else {
-    BUTTON.disabled = true;
-  }
+    if (INPUT.value.trim() !== "") {
+        BUTTON.disabled = false;
+    } else {
+        BUTTON.disabled = true;
+    }
 });
 
 let juegoTerminado = false;
 
 BUTTON.addEventListener("click", () => {
-  if (juegoTerminado) {
-    return;
-  }
+    if (juegoTerminado) {
+        return;
+    }
 
-  const INTENTO = leerIntento();
-  const GRID = document.getElementById("grid");
+    const INTENTO = leerIntento();
+    const GRID = document.getElementById("grid");
 
 
-  if (INTENTO.length !== 5) {
-    mostrarMensaje("Debe usar 5 letras");
-    return;
-  }
+    if (INTENTO.length !== 5) {
+        mostrarMensaje("Debe usar 5 letras");
+        return;
+    }
 
-  let row = document.createElement("div");
-  row.className = "row";
-  for (let i in PALABRA) {
-    if (PALABRA[i] === INTENTO[i]) {
-      let cuadroLetra = armarLetra(INTENTO[i], "green");
-      row.appendChild(cuadroLetra);
-    } else if (PALABRA.includes(INTENTO[i])) {
-      let cuadroLetra = armarLetra(INTENTO[i], "blue");
-      row.appendChild(cuadroLetra);
+    let row = document.createElement("div");
+    row.className = "row";
+    for (let i in PALABRA) {
+        if (PALABRA[i] === INTENTO[i]) {
+            let cuadroLetra = armarLetra(INTENTO[i], "green");
+            row.appendChild(cuadroLetra);
+        } else if (PALABRA.includes(INTENTO[i])) {
+            let cuadroLetra = armarLetra(INTENTO[i], "blue");
+            row.appendChild(cuadroLetra);
+        } else {
+            let cuadroLetra = armarLetra(INTENTO[i], "gray");
+            row.appendChild(cuadroLetra);
+        }
+    }
+    GRID.appendChild(row);
+    if (INTENTO === PALABRA) {
+        terminar("<h1>GANASTE!</h1>");
+        juegoTerminado = true;
     } else {
-      let cuadroLetra = armarLetra(INTENTO[i], "gray");
-      row.appendChild(cuadroLetra);
+        intentos--;
+        if (intentos === 0) {
+            terminar("<h1>PERDISTE!</h1>");
+            juegoTerminado = true;
+        }
     }
-  }
-  GRID.appendChild(row);
-  if (INTENTO === PALABRA) {
-    terminar("<h1>GANASTE!</h1>");
-    juegoTerminado = true;
-  } else {
-    intentos--;
-    if (intentos === 0) {
-      terminar("<h1>PERDISTE!</h1>");
-      juegoTerminado = true;
-    }
-  }
 
 
-  if (intentos === 0 || INTENTO === PALABRA) {
-    BUTTON.disabled = true;
-    RESTART_BUTTON.style.display = "inline"; 
-  }
+    if (intentos === 0 || INTENTO === PALABRA) {
+        BUTTON.disabled = true;
+        RESTART_BUTTON.style.display = "inline";
+    }
 });
 
 RESTART_BUTTON.addEventListener("click", reiniciarJuego);
 
 function leerIntento() {
-  return document.getElementById("guess-input").value.toUpperCase();
+    return document.getElementById("guess-input").value.toUpperCase();
 }
 
 function terminar(mensaje) {
-  document.getElementById("mensaje").innerHTML = mensaje;
+    document.getElementById("mensaje").innerHTML = mensaje;
 }
 
 function armarLetra(letra, color) {
-  let span = document.createElement("span");
-  span.className = "letter";
-  span.innerHTML = letra;
-  span.style.backgroundColor = color;
-  return span;
+    let span = document.createElement("span");
+    span.className = "letter";
+    span.innerHTML = letra;
+    span.style.backgroundColor = color;
+    return span;
 }
 
 window.addEventListener("load", () => {
-  const popup = document.getElementById("popup");
-  popup.style.display = "block";
+    const popup = document.getElementById("popup");
+    popup.style.display = "block";
 });
 
 function cerrarPopup() {
-  const popup = document.getElementById("popup");
-  popup.style.display = "none";
+    const popup = document.getElementById("popup");
+    popup.style.display = "none";
 }
 
 function mostrarMensaje(mensaje) {
-  const mensajeElement = document.getElementById("mensaje");
-  mensajeElement.textContent = mensaje;
-  setTimeout(() => {
-    mensajeElement.textContent = "";
-  }, 2000);
+    const mensajeElement = document.getElementById("mensaje");
+    mensajeElement.textContent = mensaje;
+    setTimeout(() => {
+        mensajeElement.textContent = "";
+    }, 2000);
 }
 
 function reiniciarJuego() {
+  RESTART_BUTTON.removeEventListener("click", reiniciarJuego);
   PALABRA = DICCIONARIO[Math.floor(Math.random() * DICCIONARIO.length)];
   intentos = 6;
   juegoTerminado = false;
 
-  
   const GRID = document.getElementById("grid");
   GRID.innerHTML = "";
 
-  
   INPUT.value = "";
-
 
   BUTTON.disabled = false;
   RESTART_BUTTON.style.display = "none";
 
-
   terminar("");
 
- 
   console.log(PALABRA);
+
+  RESTART_BUTTON.addEventListener("click", reiniciarJuego);
 }
